78bf0eb22a61ed5ac909149b3556e8d614c72082
PilarGuzz <94364417+PilarGuzz@users.noreply.github.com>
2023-02-15 21:00:46 +0100

    formularios login y registro con validaciones

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('myForm') myForm!: NgForm;
+  
+    username!: string;
+    password!: string;
+  
+  
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  notValidUsername(): boolean{
+    return this.myForm?.controls['username']?.invalid && 
+        this.myForm?.controls['username']?.touched
+  }
+  notValidPassword(): boolean{
+    return this.myForm?.controls['password']?.invalid && 
+        this.myForm?.controls['password']?.touched
+  }
+  onSubmit(){
+
+    console.log(`Username: ${this.password} Password: ${this.password}`)
+      this.myForm.resetForm();
+
   }
 
 }
--- src/app/auth/registrer/registrer.component.ts
+++ src/app/auth/registrer/registrer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup,  ValidatorFn,  Validators } from '@angular/forms';
 
 
 @Component({
@@ -12,15 +12,47 @@ export class RegistrerComponent implements OnInit {
   myForm: FormGroup = this.fb.group({
     username: ['', [Validators.required, Validators.minLength(3)]],
     email: ['', [Validators.required, Validators.minLength(3)]],
-    birth: ['', [Validators.required ]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-    repeatpassword: ['', [Validators.required, Validators.minLength(6)]]
+    birth: ['', [Validators.required, this.ageValidator(16) ]],
+    password: ['', [Validators.required, Validators.minLength(6), this.validatePassword]],
+    repeatpassword: ['', [Validators.required, this.matchPassword]]
   })
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
+
+  validatePassword(control: FormControl) {
+    const password = control.value;
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const valid = hasUppercase && hasLowercase && hasNumber;
+    return valid ? null : { invalidPassword: true };
+  }
+
+  matchPassword(control: FormControl) {
+    const passwordControl = control.root.get('password');
+    if (!passwordControl) {
+      return null;
+    }
+    const password = passwordControl.value;
+    const confirmPassword = control.value;
+    return password === confirmPassword ? null : { mismatch: true };
+  }
+
+
+   ageValidator(minAge: number): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      const birthday = new Date(control.value);
+      const ageDiffMs = Date.now() - birthday.getTime();
+      const ageDate = new Date(ageDiffMs);
+      const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+      return age >= minAge ? null : { 'minAge': {value: age} };
+    };
+  }
+
+
   isValidField(field:string){
     return this.myForm.controls[field].errors
     && this.myForm.controls[field].touched
